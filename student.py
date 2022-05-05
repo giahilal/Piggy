b39c500071736b8d8b6069dacebaa208b9c150a7
Gia Hilal <102991024+giahilal@users.noreply.github.com>
2022-05-05 18:27:39 +0000

    square

--- student.py
+++ student.py
@@ -60,7 +60,7 @@ class Piggy(PiggyParent):
       for edge in range(4):
         self.fwd()
         time.sleep(1)
-        self.turn_by_deg(80)
+        self.turn_by_deg(75)
         time.sleep(0.8)
       self.stop()
       
