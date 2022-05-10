a897ebc302a1fafded34997e52eb7ccc9f1e2102
Gia Hilal <102991024+giahilal@users.noreply.github.com>
2022-05-10 18:25:21 +0000

    box

--- student.py
+++ student.py
@@ -123,7 +123,10 @@ class Piggy(PiggyParent):
           self.turn_by_deg(180)
 
     def move_around_box(self):
-      if self.safe_to_dance() == False:
+      if self.safe_to_dance():
+        self.fwd()
+        time.sleep(0.5)
+      else:
         self.fwd()
         time.sleep(2)
         self.turn_by_deg(80)
